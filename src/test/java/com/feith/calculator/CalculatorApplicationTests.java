83e9f9f507b203fa887afd3a20b698a378b9584d
martfeit <martin.feith@arcor.de>
2023-11-21 13:18:45 +0100

    unit test added

--- src/test/java/com/feith/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/feith/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,20 @@
 package com.feith.calculator;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
+	private Calculator calculator = new Calculator();
+	
 	@Test
-	void contextLoads() {
+	//void contextLoads() {
+	void testSum(){
+		 assertEquals(5,calculator.sum(2,3));
 	}
-
 }
+
+
