c5d7d35978ec1da1df311c2530294f07ebb72878
martfeit <martin.feith@arcor.de>
2023-11-22 15:28:40 +0100

    DisplayName for Tests added

--- src/test/java/com/feith/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/feith/calculator/CalculatorApplicationTests.java
@@ -2,6 +2,7 @@ package com.feith.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,11 +12,13 @@ class CalculatorApplicationTests {
 	private Calculator calculator = new Calculator();
 	
 	@Test
+	@DisplayName("Simple Addition Test 1:")
 	void testSum5(){
 		assertEquals(5, calculator.sum(2,3));
 	}
 
 	@Test
+	@DisplayName("Simple Addition Test 2:")
 	void testSum10(){
 		assertEquals(10, calculator.sum(2,8));
     }
