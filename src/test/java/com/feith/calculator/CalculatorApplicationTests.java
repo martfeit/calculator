b58b5971286c619a3616fcab5d71f36851e5fda5
martfeit <martin.feith@arcor.de>
2023-11-27 11:14:26 +0100

    Test extented

--- src/test/java/com/feith/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/feith/calculator/CalculatorApplicationTests.java
@@ -13,7 +13,7 @@ class CalculatorApplicationTests {
 	private Calculator calculator = new Calculator();
 
 	@ParameterizedTest
-	@CsvSource({"2,8,10", "2,3,5"})
+	@CsvSource({"2,8,10", "2,3,5", "5,6,11"})
 	@DisplayName("Sum of two integers")
 	void testSum10(int first, int second, int expected){
 		assertEquals(expected, calculator.sum(first, second));
